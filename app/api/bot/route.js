e5c92030ac645befdacdbf2193403f2e97b98f98
VasylHorban <nagatolt427@gmail.com>
2024-12-22 19:11:13 +0200

    fix bot

--- app/api/bot/route.js
+++ app/api/bot/route.js
@@ -9,29 +9,36 @@ export async function POST(req) {
       return new Response(JSON.stringify({ message: 'All fields are required' }), { status: 400 });
     }
 
-    const botToken = process.env.TELEGRAM_BOT_TOKEN; // Ensure these are set in .env.local
-    const chatId = process.env.TELEGRAM_CHAT_ID; // Ensure these are set in .env.local
+    const botToken = process.env.TELEGRAM_BOT_TOKEN;
+    const chatId = process.env.TELEGRAM_CHAT_ID;
 
-    const text = `New Form Submission:\n\nName: ${name}\nEmail: ${email}\nMessage: ${message || '-'}`;
+    if (!botToken || !chatId) {
+      throw new Error('Bot token or chat ID not configured.');
+    }
+
+    const text = `New Form Submission:\n\nName: ${name}\nEmail: ${email}\nMessage: ${message}`;
+
+    console.log(text, message, name, botToken, chatId);
 
     const telegramResponse = await fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         chat_id: chatId,
-        text,
+        text: text,
       }),
     });
 
-    console.log(telegramResponse, ' telegramResponse');
+    const result = await telegramResponse.json();
 
     if (!telegramResponse.ok) {
-      throw new Error(`Telegram API Error: ${telegramResponse.statusText}`);
+      console.error('Telegram API response:', result);
+      throw new Error(`Telegram API Error: ${result.description}`);
     }
 
     return new Response(JSON.stringify({ message: 'Message sent successfully' }), { status: 200 });
   } catch (error) {
     console.error('Error sending message to Telegram:', error);
-    return new Response(JSON.stringify({ message: 'Internal server error' }), { status: 500 });
+    return new Response(JSON.stringify({ message: 'Internal server error', error: error.message }), { status: 500 });
   }
 }
