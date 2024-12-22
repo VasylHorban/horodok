10df0436d5a43aeba8dead9ce5da5c23599698e6
VasylHorban <nagatolt427@gmail.com>
2024-12-22 17:26:15 +0200

    fix

---
+++ src/graphql/client.js
@@ -0,0 +1,7 @@
+import { GraphQLClient } from 'graphql-request';
+
+export const graphQLClient = new GraphQLClient(`${process.env.CONTENTFUL_URL}/${process.env.CONTENTFUL_SPACE_ID}`, {
+  headers: {
+    Authorization: `Bearer ${process.env.CONTENTFUL_ACCESS_KEY}`,
+  },
+});
